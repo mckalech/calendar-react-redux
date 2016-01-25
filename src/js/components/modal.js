0ca4642d734009a2da1efd7da919939f21c8f306
k.kalchenko <k.kalchenko@corp.mail.ru>
2016-01-25 18:06:49 +0300

    modal styles

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
-export default class Modal extends Component {
+class Modal extends Component {
 	render() {
+		let date = this.props.date;
 		return (
-			<div className="columns six">
-				{this.props.params.date}
+			<div className="b-popup">
+				<div className="b-popup__window">
+					<span className="b-popup__close">x</span>
+					<div className="b-popup__date"></div>
+					<div className="b-popup__title">
+						<input type="text" placeholder="Заголовок" name="title" />
+					</div>
+					<div className="b-popup__description">
+						<textarea name="description" placeholder="Описание"></textarea>
+					</div>
+					<p className="b-popup__warning-wrapper">
+						<span className="b-popup__warning">Для сохранения заполните все поля</span>
+					</p>
+					<span className="b-popup__btn b-popup__btn_save">Сохранить</span>
+					<span className="b-popup__btn b-popup__btn_delete">Удалить</span>
+				</div>
 			</div>
 		)
 	}
 }
 
+function mapStateToProps(state, ownProps) {
+	return{
+		date: ownProps.params.date
+	}
+
+
+}
+
+export default connect(mapStateToProps)(Modal);
